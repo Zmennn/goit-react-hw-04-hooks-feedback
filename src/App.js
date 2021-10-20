108900c6e4498a898989999a7963b780d8bd4630
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-20 17:44:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Section from './components/Section';
 
 export default function App() {
-  let countTotalFeedback = useRef(0);
-  let countPositiveFeedbackPercentage = useRef(0);
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  // const [state, dispatch] = useReducer(countReducer, {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // })
 
   const addGood = () => {
     setGood(prev => prev + 1);
@@ -20,12 +23,13 @@ export default function App() {
     setBad(prev => prev + 1);
   };
 
-  useEffect(() => {
-    countTotalFeedback.current = good + neutral + bad;
-    countPositiveFeedbackPercentage.current = Math.round(
-      (good / countTotalFeedback.current) * 100,
-    );
-  });
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
 
   return (
     <>
@@ -37,8 +41,8 @@ export default function App() {
         good={good}
         neutral={neutral}
         bad={bad}
-        total={countTotalFeedback.current}
-        positivePercentage={countPositiveFeedbackPercentage.current}
+        total={countTotalFeedback()}
+        positivePercentage={countPositiveFeedbackPercentage()}
       />
     </>
   );
